0d53f6683bc46d737c119838655e51e0eb1e51d8
Adheesh Verma <58031855+AdheeshVerma@users.noreply.github.com>
2025-01-28 09:20:18 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -14,7 +14,11 @@ int main() {
         printf("%.0f",a*b);
     }
     else if(c=='/'){
-        printf("%.0f",a/b);
+        if(b==0){
+            printf("error");
+        }
+        else{
+        printf("%.0f",a/b);}
     }
     return 0;
 }
