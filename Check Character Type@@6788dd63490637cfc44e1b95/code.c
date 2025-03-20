e314cbf6e590db5c2a036f317692514fe3bc5ca5
Adheesh Verma <58031855+AdheeshVerma@users.noreply.github.com>
2025-03-20 21:22:16 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -8,7 +8,7 @@ int main(){
             printf("Vowel");
         }
         else{
-            printf("Consonent");
+            printf("Consonant");
         }
     }
     else if(a>=48 && a<=57){
