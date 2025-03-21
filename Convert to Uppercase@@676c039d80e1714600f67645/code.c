8877f7b22ddf1befe8f8cf318d875a64a4333ac3
Adheesh Verma <58031855+AdheeshVerma@users.noreply.github.com>
2025-03-21 19:05:33 +0530

    Solution to Convert to Uppercase

--- Convert to Uppercase@@676c039d80e1714600f67645/code.c
+++ Convert to Uppercase@@676c039d80e1714600f67645/code.c
@@ -2,7 +2,7 @@
 #include<string.h>
 #include<ctype.h>
 void toupp(char* str){
-    int count = strlen(str)-1;
+    int count = strlen(str);
     for (int i = 0; i < count; i++)
     {
         str[i]= toupper(str[i]);
