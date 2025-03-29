c0c0c6a05c6cccbf8f82027adcb7980ed1f3e822
Adheesh Verma <58031855+AdheeshVerma@users.noreply.github.com>
2025-03-29 23:12:59 +0530

    Solution to Digit Increment to Next Prime

---
+++ Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
@@ -0,0 +1,55 @@
+// Your code here...
+int joiner(int* arr,n){
+    int res = 0;
+    for(int i=0;i<count;i++){
+        int a = arr[i];
+        res=res*10+a;
+    }
+    return res;
+}
+void flipper(int* arr,a,b){
+int temp = arr[a];
+arr[a]=arr[b];
+arr[b]=temp;
+a++;
+b--;
+}
+int incrementToPrimeDigits(int n){
+    int a=n,count=0;
+    while(a){
+        count++;
+        a/=10;
+    }
+    int arr[count];
+    for(int i=0,i<count;i++){
+        int r=n%10;
+        arr[i]=r;
+        n/=10;
+    }
+    for(int i=0;i<count;i++){
+        if(arr[i]==0)
+        arr[i]=2;
+        else if(arr[i]==1)
+        arr[i]==2;
+        else if(arr[i]==2)
+        arr[i]==3;
+        else if(arr[i]==3)
+        arr[i]==5;
+        else if(arr[i]==4)
+        arr[i]==5;
+        else if(arr[i]==5)
+        arr[i]==7;
+        else if(arr[i]==6)
+        arr[i]==7;
+        else if(arr[i]==7)
+        arr[i]==2;
+        else if(arr[i]==8)
+        arr[i]==2;
+        else if(arr[i]==9)
+        arr[i]==2;
+        
+    }
+    flipper(arr,0,count);
+    int res = joiner(arr,count);
+    return res;
+}
