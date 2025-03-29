145c977ef1720f152d51603724ed7a0e54c972d3
Adheesh Verma <58031855+AdheeshVerma@users.noreply.github.com>
2025-03-30 01:01:44 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,6 +1,14 @@
+int counter(char* str){
+    int i=0,count=0;
+    while(str[i]!='\0'){
+        count++;
+        i++;
+    }
+    return count;
+}
 void caesarCipher(char *message, int shift, char *encrypted)
 {
-    int len = strlen(message);
+    int len = coutner(message);
     for (int i = 0; i < len; i++)
     {
         char r = message[i];
