0c39b1a00c8693efc5b30a88bfc16b940991926e
Adheesh Verma <58031855+AdheeshVerma@users.noreply.github.com>
2025-03-29 23:45:44 +0530

    Solution to Game Invitation Chain – Tracking the Likes

---
+++ Game Invitation Chain – Tracking the Likes@@67de00956ac3fec9afb90e1d/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+int totalLikes(int n){
+    if(n==1){
+        return 2;
+    }
+    else{
+        int shared = 5,res=0;
+        for(int i=0;i<n){
+            int likes = shared/2;
+            res+=likes;
+            shared = likes*3;
+        }
+        return res;
+    }
+}
