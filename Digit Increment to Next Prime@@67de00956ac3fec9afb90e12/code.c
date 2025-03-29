63caec9885ed2c9934ac171ffef32e0d2165daa2
Adheesh Verma <58031855+AdheeshVerma@users.noreply.github.com>
2025-03-29 23:22:17 +0530

    Solution to Digit Increment to Next Prime

--- Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
+++ Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
@@ -26,22 +26,22 @@ int incrementToPrimeDigits(int n){
         arr[i]=2;}
         else if(arr[i]==1){
         arr[i]==2;}
-        else if(arr[i]==2)
-        arr[i]==3;
-        else if(arr[i]==3)
-        arr[i]==5;
-        else if(arr[i]==4)
-        arr[i]==5;
-        else if(arr[i]==5)
-        arr[i]==7;
-        else if(arr[i]==6)
-        arr[i]==7;
-        else if(arr[i]==7)
-        arr[i]==2;
-        else if(arr[i]==8)
-        arr[i]==2;
-        else if(arr[i]==9)
-        arr[i]==2;
+        else if(arr[i]==2){
+        arr[i]==3;}
+        else if(arr[i]==3){
+        arr[i]==5;}
+        else if(arr[i]==4){
+        arr[i]==5;}
+        else if(arr[i]==5){
+        arr[i]==7;}
+        else if(arr[i]==6){
+        arr[i]==7;}
+        else if(arr[i]==7){
+        arr[i]==2;}
+        else if(arr[i]==8){
+        arr[i]==2;}
+        else if(arr[i]==9){
+        arr[i]==2;}
         
     }
         for(int i=0;i<count;i++){
